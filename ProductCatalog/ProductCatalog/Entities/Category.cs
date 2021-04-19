ff123e55ee5fe0d6cde255e378ce1ccbcd426010
Rohit kumar pandey <80757398+pandeyrohit1234@users.noreply.github.com>
2021-04-18 23:08:34 -0700

    product

---
+++ ProductCatalog/ProductCatalog/Entities/Category.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Entities
+{
+    public class Category
+    {
+        // public static int Id { get; set; }
+        public  static int Id { get; set; }
+        public string Name { get; set; }
+        [StringLength(4)]
+        public string ShortCode { get; set;}
+        public string Description { get; set; }
+
+        public static List<Category> printcategory = new List<Category>();
+        public Category() 
+        {
+            Id = Id + 1;
+        }
+
+        public void addCategory()
+        {
+            Console.WriteLine("Enter Name of Category");
+            Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Short Code:  Max 4 charachters");
+            ShortCode = Console.ReadLine();
+
+            Console.WriteLine("Enter Description for Category");
+            Description = Console.ReadLine();
+
+        }
+
+        public static void showCategory()
+        {
+            foreach(Category i in printcategory)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ProductCatalog/ProductCatalog/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCatalog.Entities
+{
+    public class Product
+    {
+        public static int Id { get; private set; }
+        public string Name { get; set; }
+        public string Manufacturer { get; set; }
+
+        [StringLength(4)]
+        public string ShortCode { get; set; }
+
+        public string Description { get; set; }
+
+        public List<Category> Categories { get; set; }
+        public int SellingPrice { get; set; }
+        public Product()
+        {
+            Id = Id + 1;
+        }
+
+    }
+}
